4ff374ddd99ac0902230b33ab731a207653e7642
tonynowater <tonynowater87@gmai..com>
2022-11-19 21:51:31 +0800

    Fix dialog overflow pixels issue

--- lib/view/add_event/add_event_page.dart
+++ lib/view/add_event/add_event_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -68,110 +67,112 @@ class _AddEventPageState extends State<AddEventPage> {
                 borderRadius: const BorderRadius.all(Radius.circular(8))),
             child: BlocBuilder<AddEventBloc, AddEventState>(
               builder: (context, state) {
-                return Column(children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child:
-                        status == AddEventStatus.add ? Text('新增') : Text('編輯'),
-                  ),
-                  const Divider(),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: OutlinedButton.icon(
-                      onPressed: () async {
-                        // TODO locale for DatePicker
-                        final pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: DateTime.now(),
-                            firstDate: DateTime(1900),
-                            lastDate: DateTime(2100));
-                        if (!mounted) {
-                          return;
-                        }
-                        if (pickedDate != null) {
-                          context
-                              .read<AddEventBloc>()
-                              .add(UpdateDateTimeEvent(pickedDate));
-                        }
-                      },
-                      icon: const Icon(Icons.edit_calendar_outlined),
-                      label: Text(DateFormat(DateFormat.YEAR_MONTH_DAY,
-                              AppConstants.defaultLocale)
-                          .format(state.memoModel.dateTime)), // Locale
+                return SingleChildScrollView(
+                  child: Column(children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child:
+                          status == AddEventStatus.add ? Text('新增') : Text('編輯'),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Scrollbar(
-                      controller: _scrollController,
-                      isAlwaysShown: true,
-                      child: TextField(
-                        controller: _textEditingController,
-                        cursorColor: Theme.of(context).focusColor,
-                        minLines: 1,
-                        maxLines: 4,
-                        scrollController: _scrollController,
-                        keyboardType: TextInputType.multiline,
-                        onChanged: (text) {
-                          context.read<AddEventBloc>().add(
-                              UpdateMemoEvent(_textEditingController.text));
+                    const Divider(),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: OutlinedButton.icon(
+                        onPressed: () async {
+                          // TODO locale for DatePicker
+                          final pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime(2100));
+                          if (!mounted) {
+                            return;
+                          }
+                          if (pickedDate != null) {
+                            context
+                                .read<AddEventBloc>()
+                                .add(UpdateDateTimeEvent(pickedDate));
+                          }
                         },
-                        decoration: const InputDecoration(
-                          icon: Icon(Icons.event_note),
-                          labelText: '事件',
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(),
+                        icon: const Icon(Icons.edit_calendar_outlined),
+                        label: Text(DateFormat(DateFormat.YEAR_MONTH_DAY,
+                                AppConstants.defaultLocale)
+                            .format(state.memoModel.dateTime)), // Locale
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Scrollbar(
+                        controller: _scrollController,
+                        isAlwaysShown: true,
+                        child: TextField(
+                          controller: _textEditingController,
+                          cursorColor: Theme.of(context).focusColor,
+                          minLines: 1,
+                          maxLines: 4,
+                          scrollController: _scrollController,
+                          keyboardType: TextInputType.multiline,
+                          onChanged: (text) {
+                            context.read<AddEventBloc>().add(
+                                UpdateMemoEvent(_textEditingController.text));
+                          },
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.event_note),
+                            labelText: '事件',
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                              child: ElevatedButton(
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              Colors.transparent),
+                                      shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                          const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.zero,
+                                              side: BorderSide()))),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('取消'))),
+                          const SizedBox(width: 20),
+                          Expanded(
                             child: ElevatedButton(
                                 style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.transparent),
                                     shape: MaterialStateProperty.all<
                                             RoundedRectangleBorder>(
                                         const RoundedRectangleBorder(
                                             borderRadius: BorderRadius.zero,
                                             side: BorderSide()))),
-                                onPressed: () {
-                                  Navigator.pop(context);
+                                onPressed: () async {
+                                  await context.read<AddEventBloc>().saveEvent();
+                                  if (!mounted) {
+                                    return;
+                                  }
+                                  Navigator.pop(context, true);
                                 },
-                                child: const Text('取消'))),
-                        const SizedBox(width: 20),
-                        Expanded(
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      const RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.zero,
-                                          side: BorderSide()))),
-                              onPressed: () async {
-                                await context.read<AddEventBloc>().saveEvent();
-                                if (!mounted) {
-                                  return;
-                                }
-                                Navigator.pop(context, true);
-                              },
-                              child: status == AddEventStatus.add
-                                  ? Text('新增')
-                                  : Text('更新')),
-                        )
-                      ],
+                                child: status == AddEventStatus.add
+                                    ? Text('新增')
+                                    : Text('更新')),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ]);
+                  ]),
+                );
               },
             ),
           ),
